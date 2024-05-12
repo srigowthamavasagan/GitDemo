fdc12916929cd2f8c65bfacb81dd0c4eb4e0cb9c
siva <sivasrigowtham2@gmail.com>
2024-05-12 22:21:23 +0530

    this is commit from another person ast

--- src/SellIntroduction.java
+++ src/SellIntroduction.java
@@ -1,20 +1,17 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-
-
 
 public class SellIntroduction {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//System.setProperty("webdriver.chrome.driver", "/Users/User/Documents/chromedriver.exe");
-		System.setProperty("webdriver.edge.driver", "/Users/User/Documents/msedgedriver.exe");
-		
-		//WebDriver driver = new ChromeDriver();
-		WebDriver driver = new EdgeDriver();
-	 driver.get("https://maven.apache.org/surefire/maven-surefire-plugin/examples/testng.html");
-		
+
+		System.setProperty("webdriver.chrome.driver", "/Users/User/Documents/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.google.com/");
+
 	}
 
 }
